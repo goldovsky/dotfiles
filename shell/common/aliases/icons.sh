004bd77ce74e25a57bfcfe70a8b64dbccee450b7
goldovsky <goldovsky@protonmail.com>
2026-05-20 10:30:23 +0200

    fix: icons and tmux new pane location

--- shell/common/aliases/icons.sh
+++ shell/common/aliases/icons.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
-# ICONS TO BE USED IN TMUX
-ICON_IHM="  "
+## nf-md-alpha_m
+ICON_TMUX="󰬁󰫺󰬂󰬅"
+export ICON_TMUX
+ICON_GOLDOVSKY="󰫴󰫼󰫹󰫱󰫼󰬃󰬀󰫸󰬆"
+export ICON_GOLDOVSKY
+ICON_IHM="󰫶󰫵󰫺 "
 export ICON_IHM
 ICON_API="󱂛 "
 export ICON_API
-ICON_CORE="  "
+ICON_CORE="  "
 export ICON_CORE
 ICON_CHAT="󰭹 "
 export ICON_CHAT
-ICON_DOCKER=" "
+ICON_DOCKER=" "
 export ICON_DOCKER
 ICON_MENU="󰍜 "
 export ICON_MENU
-ICON_APACHE="  "
+ICON_APACHE=" "
 export ICON_APACHE
-ICON_SHELL="  "
+ICON_SHELL=" "
 export ICON_SHELL
-ICON_GOLDOVSKY="󰫴󰫼󰫹󰫱󰫼󰬃󰬀󰫸󰬆"
-export ICON_GOLDOVSKY
-ICON_CANADA=" "
+ICON_CANADA=" "
 export ICON_CANADA
 ICON_LYS="󱌃 "
 export ICON_LYS
 ICON_DURATION="󰄉 "
 export ICON_DURATION
-ICON_FOLDER=" "
+ICON_FOLDER="󰉋 "
 export ICON_FOLDER
-ICON_NVIM=" "
+ICON_NVIM=" "
 export ICON_NVIM
-ICON_CODE=" "
+ICON_CODE=" "
 export ICON_CODE
 ICON_GIT="⸎ "
 export ICON_GIT
-ICON_MARKDOWN=" "
+ICON_MARKDOWN=" "
 export ICON_MARKDOWN
-ICON_TMUX="󰬁󰫺󰬂󰬅"
-export ICON_TMUX
--- tmux/.config/tmux/keybindings.tmux
+++ tmux/.config/tmux/keybindings.tmux
@@ -12,5 +12,5 @@ bind C-c run "tmux save-buffer - | xclip -i -sel clip"
 bind C-v run "tmux set-buffer $(xclip -o -sel clip); tmux paste-buffer"
 
 # split window
-bind - split-window -v # horizontal split (pane below)
-bind | split-window -h # vertical split (pane to the right)
+bind - split-window -v -c "#{pane_current_path}" # horizontal split (pane below)
+bind | split-window -h -c "#{pane_current_path}" # vertical split (pane to the right)
